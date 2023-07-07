d429b21ca9b4dc84ebf1614cffd101072c66d72f
Hadrien <hadrien.houtmann@ik.me>
2023-07-07 09:26:26 +0200

    add missing method to noop storage

--- noop.go
+++ noop.go
@@ -15,4 +15,7 @@ type noop struct{}
 func (noop) Save(ctx context.Context, content io.Reader, path string) error { return nil }
 func (noop) Stat(ctx context.Context, path string) (*Stat, error)           { return nil, nil }
 func (noop) Open(ctx context.Context, path string) (io.ReadCloser, error)   { return nil, nil }
-func (noop) Delete(ctx context.Context, path string) error                  { return nil }
+func (noop) OpenWithStat(ctx context.Context, path string) (io.ReadCloser, *Stat, error) {
+	return nil, nil, nil
+}
+func (noop) Delete(ctx context.Context, path string) error { return nil }
